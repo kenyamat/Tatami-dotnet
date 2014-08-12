682419285ffa390ad2d9a12164b63ad9a2a6c750
Kenya Matsumoto <kmatsumoto@mdsol.com>
2014-08-12 19:08:01 +0900

    Added sample project.

---
+++ samples/SampleTest/Wikipedia/WikipediaTests.cs
@@ -0,0 +1,33 @@
+﻿namespace SampleTest.Wikipedia
+{
+    using System.IO;
+    using System.Threading.Tasks;
+    using Tatami;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class WikipediaTests
+    {
+        /// <summary>
+        /// Test United States page
+        /// http://en.wikipedia.org/wiki/United_States
+        /// </summary>
+        [TestMethod]
+        public async Task TestWikipediaWithUnitedStatesPage()
+        {
+            // Arrange
+            var testCasesCsv = File.ReadAllText(@"Wikipedia\Resources\Test_United_States.csv");
+            var baseUriMappingXml = File.ReadAllText(@"Wikipedia\Resources\BaseUriMapping.xml");
+            var userAgentMappingXml = File.ReadAllText(@"Wikipedia\Resources\UserAgentMapping.xml");
+
+            // Act
+            var result = await TestExecutor.Test(testCasesCsv, baseUriMappingXml, userAgentMappingXml);
+
+            // Assert
+            if (!string.IsNullOrWhiteSpace(result.FailedMessage))
+            {
+                Assert.Fail(result.FailedMessage);
+            }
+        }
+    }
+}
---
+++ samples/SampleTest/YahooWeather/YahooWeatherTests.cs
@@ -0,0 +1,36 @@
+﻿namespace SampleTest.YahooWeather
+{
+    using System.IO;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Tatami;
+
+    [TestClass]
+    public class YahooWeatherTests
+    {
+        private static readonly string baseUriMappingXml = File.ReadAllText(@"YahooWeather\Resources\BaseUriMapping.xml");
+        private static readonly string userAgentMappingXml = File.ReadAllText(@"YahooWeather\Resources\UserAgentMapping.xml");
+
+        /// <summary>
+        /// Test New york page
+        /// https://weather.yahoo.com/us/ny/new-york-2459115/
+        /// </summary>
+        [TestMethod]
+        public async Task TestNewYork()
+        {
+            // Arrange
+            var testCasesCsv = File.ReadAllText(@"YahooWeather\Resources\Test_NewYork.csv");
+
+            // Act
+            var result = await TestExecutor.Test(testCasesCsv, baseUriMappingXml, userAgentMappingXml, null);
+            var failedMessage = result.FailedMessage;
+
+            // Assert
+            // var debug = result.ResultMessage;
+            if (!string.IsNullOrWhiteSpace(failedMessage))
+            {
+                Assert.Fail(failedMessage);
+            }
+        }
+    }
+}
